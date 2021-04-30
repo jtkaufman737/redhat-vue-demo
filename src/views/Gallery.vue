de1ace392d44e76a06bd33ca1430c57a12a4e132
jtkaufman737 <jtkaufman737@gmail.com>
2021-04-30 10:51:08 -0400

    End of changes to base

---
+++ src/views/Gallery.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="Gallery">
+    <h1>Pokemon Gallery</h1>
+    <h6><pre>// TODO gallery</pre></h6>
+    <main>
+
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Gallery',
+  data() {
+    return {
+
+    }
+  },
+  methods: {},
+}
+</script>
+<style>
+main {
+  display: grid;
+
+}
+</style>
