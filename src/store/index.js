173d871f69806a1a3f0a65fe993e8fc295da1541
jtkaufman737 <jtkaufman737@gmail.com>
2021-05-04 08:43:11 -0400

    Reset base

--- src/store/index.js
+++ src/store/index.js
@@ -4,33 +4,9 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    pokemon:[
-      { id: 1, name: "Caterpie", img: "https://i.imgur.com/A21Gpql.png" },
-      { id: 2, name: "Snorlax", img: "https://i.imgur.com/TGf6qB8.png" },
-      { id: 3, name: "Pikachu", img: "https://i.imgur.com/fmMERCo.png" },
-      { id: 4, name: "Abra", img: "https://i.imgur.com/f59APqT.png" },
-      { id: 5, name: "Dratini", img: "https://i.imgur.com/K9DxFvF.png" },
-      { id: 6, name: "Charmander", img: "https://i.imgur.com/KuZEzvo.png" },
-      { id: 7, name: "Eevee", img: "https://i.imgur.com/zHg4gnJ.png" },
-      { id: 8, name: "Mankey", img: "https://i.imgur.com/FQyOh1f.png" },
-      { id: 9, name: "Meowth", img: "https://i.imgur.com/MVntz6D.png" },
-      { id: 10, name: "Pidgey", img: "https://i.imgur.com/Rgc0OHA.png" },
-      { id: 11, name: "Bulbasaur", img: "https://i.imgur.com/e7VtLbo.png" },
-      { id: 12, name: "Jigglypuff", img: "https://i.imgur.com/SU7yF1f.png" }
-    ]
-  },
-  mutations: {
-    removePokemon: (state, id) => {
-      state.pokemon = state.pokemon.filter(p => {
-        return p.id != id;
-      })
-    }
-  },
-  actions: {
-    runRemovePokemon: ({ commit }, id) => {
-      commit("removePokemon", id);
-    }
+  state: {},
+  mutations: {},
+  actions: {}
   },
   modules: {}
 })
--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,74 +1,18 @@
 <template>
   <div id="Gallery">
     <h1>Pokemon Gallery</h1>
-    <h4>Description</h4>
-    <button @click="toggleEditable">
-      <span v-if="editable">Save</span>
-      <span v-else>Edit</span>
-    </button>
-    <div v-if="editable">
-      <textarea v-model="description"></textarea>
-    </div>
-    <div v-else>
-      {{ description }}
-    </div>
-    <h4>Regions our Pokemon are from</h4>
-    <ul>
-      <li
-        :id="region"
-        v-for="region in regions"
-        @mouseover="addRegionColor(region)"
-        @mouseleave="clearRegionColor(region)"
-      >{{ region }}</li>
-    </ul>
-    <h4>Gallery</h4>
-    <div id="main">
-      <div class="grid">
-        <div class="card" v-for="pokemon in pokemon" @click="removePokemon(pokemon.id)">
-          <img :src="pokemon.img"/>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
-import store from '@/store';
-
 export default {
   name: 'Gallery',
   data() {
     return {
-      editable: false,
-      description:"Test description of gallery",
-      regions: ["Johto", "Kanto", "Sinoh", "Hoenn"],
-      buttonColors: {
-        Johto: "#27c476",
-        Kanto: "#27c4bc",
-        Sinoh: "#27a5c4",
-        Hoenn: "#757cfa",
-      }
     }
   },
   methods: {
-    toggleEditable() {
-      this.editable = !this.editable
-    },
-    addRegionColor(region) {
-      document.getElementById(region).style.backgroundColor = this.buttonColors[region];
-    },
-    clearRegionColor(region) {
-      document.getElementById(region).style.backgroundColor = "";
-    },
-    removePokemon(id) {
-      store.dispatch('runRemovePokemon', id);
-    }
   },
-  computed: {
-    pokemon() {
-      return store.state.pokemon
-    }
-  }
 }
 </script>
 <style scoped>
